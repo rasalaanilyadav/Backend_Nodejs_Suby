511aff6ba291afdee54cd488a710a5ec21779fa5
rasalaanilyadav <rasalasehitha@gmail.com>
2025-12-25 19:16:45 +0530

    small error mistake

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,4 @@
-const Product = require("../models/product");
+const Product = require("../models/Product");
 const multer = require("multer");
 const Firm = require('../models/Firm')
 const path = require('path');
